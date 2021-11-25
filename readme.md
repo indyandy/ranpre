a4db69d0304783ab0599230f4b61c49ac764b303
indyandy <90049986+indyandy@users.noreply.github.com>
2021-11-25 11:47:39 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+#Ran Pre - program for adding random prefix to existing file names.#
---
+++ source/RanPre.java
@@ -0,0 +1,20 @@
+public class RanPre {
+
+   /* Program to add Random string
+    * to existing file names.
+    */
+
+   public static void main(String []args) {
+     myriware_logo();
+   }
+
+   public static void myriware_logo(){
+     System.out.println("                     _");
+     System.out.println("                    :_;");
+     System.out.println(",-.,-.,-..-..-..--. .-..-..-..-. .--.  .--.  .--.");
+     System.out.println(": ,. ,. :: :; :: ..': :: `; `; :' .; ; : ..'' '_.'");
+     System.out.println(":_;:_;:_;`._. ;:_;  :_;`.__.__.'`.__,_;:_;  `.__.'");
+     System.out.println("          .-. :");
+     System.out.println("          `._.'");
+   }
+}
